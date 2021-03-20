65120f53bcf692758e6d79979f350967342e3638
Sunishka Sharma <73639308+sunishka-30@users.noreply.github.com>
2021-03-20 08:56:07 +0530

    Delete Loop/Basic Programs directory

--- Loop/Basic Programs/123combinations.py
+++
@@ -1,7 +0,0 @@
-print("Six Combinations of 123 with distinct numbers are :-")
-for i in range(1,4):
-    for j in range(1,4):
-        for k in range(1,4):
-            if i==j or j==k or k==i:
-                continue
-            print(i, j, k, sep='')
