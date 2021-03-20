9418fb641f03708a936b267654f02e4d966c111c
Sunishka Sharma <73639308+sunishka-30@users.noreply.github.com>
2021-03-20 09:15:34 +0530

    Add files via upload

---
+++ Loop/Basic Programs/Prime.py
@@ -0,0 +1,10 @@
+n = int(input("Enter a number.\n"))
+f=0
+for i in range (2,n//2):
+    if n%i == 0:
+        f=1
+        break
+if f==1:
+    print(n,"is not a prime number.")
+else:
+    print(n,"is a prime number.")
